ffd09ce6d7974cbfd38147b0203eca30a82ffd53
theperfumestudio <theperfumestudiolondon@gmail.com>
2025-09-10 15:36:15 +0100

    Add files via upload

---
+++ js/countdown.js
@@ -0,0 +1,35 @@
+CountDownTimer('12/31/2025 12:00 AM', 'countdown');
+
+function CountDownTimer(dt, id)
+{
+    var end = new Date(dt);
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+
+            clearInterval(timer);
+            document.getElementById(id).innerHTML = 'EXPIRED!';
+
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        $("#days").find('.number').text(days);
+        $("#hours").find('.number').text(hours);
+        $("#minutes").find('.number').text(minutes);
+        $("#seconds").find('.number').text(seconds) ;
+    }
+
+    timer = setInterval(showRemaining, 1000);
+}
